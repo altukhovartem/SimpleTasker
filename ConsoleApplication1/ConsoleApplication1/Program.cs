a2e37dd09684b11f13f649203d4c0cdfcc3a119d
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-03-30 21:32:31 +0300

    First commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Context currentDbContext = new Context())
+            {
+                Task task = new Task()
+                {
+                    taskID = 1,
+                    createdDate = new DateTime(2007, 2, 2),
+                    title = "Task1"
+                };
+
+                currentDbContext.TaskSet.Add(task);
+                currentDbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Task
+    {
+        public int taskID { get; set; }
+        public string title { get; set; }
+        public DateTime createdDate { get; set; }
+    }
+}
