b93627129e11d4a873a6dca12f4c93b5f4fe785b
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-03-30 22:04:28 +0300

    Creates simple console UI

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,15 +12,35 @@ namespace ConsoleApplication1
         {
             using (Context currentDbContext = new Context())
             {
-                Task task = new Task()
+                Console.WriteLine("*** Tasker 3000 ***");
+                Console.WriteLine("Available command for now ... Press nessecety button");
+                while (true)
                 {
-                    taskID = 1,
-                    createdDate = new DateTime(2007, 2, 2),
-                    title = "Task1"
-                };
+                    Console.WriteLine("1 - {0}\n2 - {1}\n3 - {2}", 
+                        "Add new task", 
+                        "Delete existing task",
+                        "Show all available task");
 
-                currentDbContext.TaskSet.Add(task);
-                currentDbContext.SaveChanges();
+                    int currentCommand = Convert.ToInt32(Console.ReadLine());
+                    switch (currentCommand)
+                    {
+                        case 1: Console.WriteLine("AddTask"); ; break;
+                        case 2: Console.WriteLine("DeleteTask"); ; break;
+                        case 3: Console.WriteLine("ShowTasks"); break;
+                        default:
+                            break;
+                    
+                }
+
+                //Task task = new Task()
+                //{
+                //    taskID = 1,
+                //    createdDate = new DateTime(2007, 2, 2),
+                //    title = "Task1"
+                //};
+
+                //currentDbContext.TaskSet.Add(task);
+                //currentDbContext.SaveChanges();
             }
         }
     }
