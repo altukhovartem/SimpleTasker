84a35c402be99bc3c9e247a15e9fa50fe56e09db
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-04-23 19:36:49 +0300

    adds new model Person, changes Task model and include changes with context
    ConsoleApplication1/ConsoleApplication1/Model/Taskcs

---
+++ ConsoleApplication1/ConsoleApplication1/Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using ConsoleApplication1.Model;
+
+namespace ConsoleApplication1.Data
+{
+    class currentContext: DbContext
+    {
+        public currentContext()
+            :base("currentConnection")
+        {
+
+        }
+
+        public DbSet<Task> TaskSet { get; set; }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Model/Task.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApplication1.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApplication1.Model
+{
+    class Task
+    {
+        public int TaskID { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DeadLine { get; set; }
+        public string Commentary { get; set; }
+
+        public Person Assigny { get; set; }
+    }
+}
