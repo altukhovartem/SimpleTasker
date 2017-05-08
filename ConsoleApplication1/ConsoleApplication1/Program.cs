f6c3472a0b7f6d6f52fea8325cc3cb4167b36645
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-05-09 00:53:28 +0300

    Add Console UI

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ConsoleApplication1.DBHelpers;
 using System.Data.Entity;
 using ConsoleApplication1.Data;
+using ConsoleApplication1.UI;
 
 namespace ConsoleApplication1
 {
@@ -13,32 +14,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            bool loop = true;
-            Console.WriteLine("*** Tasker 3000 ***");
-            Console.WriteLine("Available command for now ... Press nessecety button");
-            while (loop)
-            {
-                Console.WriteLine("1 - {0}\n2 - {1}\n3 - {2}\n4 - {3}\n5 - {4}",
-                    "Add new task",
-                    "Delete existing task",
-                    "Show all available task",
-                    "Add new User",
-                    "Show all available users");
-
-                int currentCommand = Convert.ToInt32(Console.ReadLine());
-                switch (currentCommand)
-                {
-                    case 1: DBAction.AddTask(); break;
-                    case 2: DBAction.DeleteTask(); break;
-                    case 3: DBAction.ShowAllTask(); break;
-                    case 4: DBAction.AddUser(); break;
-                    case 5: DBAction.ShowAllUsers(); break;
-                    case 0: loop = false; break;
-                    default:
-                        break;
-                }
-            }
+            ConsoleUI.MainConsoleUI();
         }
+
     }
 }
 
